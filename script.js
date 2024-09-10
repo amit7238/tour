49babd7a95ab8b4af3b9dfafdd6727242550cc09
amit7238 <amitmourya62@gmail.com>
2024-09-10 15:44:55 +0530

    amit

---
+++ script.js
@@ -0,0 +1,15 @@
+let main = document.querySelector('.main1')
+console.log(main);
+
+let chld= main.children
+
+console.log(chld[1])
+
+
+let sib=chld.nextEementSibling;
+let child2=sib.children;
+console.log(sib.children)
+console.log(child2[1].parentElement)
+
+
+
